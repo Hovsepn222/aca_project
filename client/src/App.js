04549e9802b48b611014c7b38d88193a782452e7
Hovsep Najarian <79749509+Hovsepn222@users.noreply.github.com>
2023-05-22 10:47:51 +0400

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import Header from './components/Header/Header';
 import CarouselComponent from './components/carousel';
 import SideNavigation from './components/sidenav';
 import { ProductItem } from './components/productItem/ProductItem';
+import { Typography } from "@mui/material";
 
 export default function App() {
   return (
     <div >
-      <Header />
       <CarouselComponent />
       <Typography variant="h2" sx={{ color: "#86c232", textAlign: "center", mb: "50px", mt: "50px" }}>Recently Added</Typography>
       <ProductItem />
